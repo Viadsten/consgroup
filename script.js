b23d382638f751ec195d8747b2d1cc043ab1de56
Viadsten <stanulevich.vg@yandex.ru>
2020-10-14 21:34:13 +0300

    .

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ var burger = document.getElementById("checkbox");
 var i = 0;
 
 burger.addEventListener('change', ()=>{
-  console.log("check");
   if (i == 0){
     $("#navigation").slideDown('300');
     i = 1;
@@ -42,4 +41,60 @@ burger.addEventListener('change', ()=>{
 })
 
 
+var slider = document.getElementById("Range");
+var output = document.getElementById("rangeOutput");
+var profit = document.getElementById("profit");
+const tsla = 1.47;
+const aapl = 1.9;
+const gold = 1.2;
+var tsla_text = document.getElementById("tsla-text");
+var aapl_text = document.getElementById("aapl-text");
+var gold_text = document.getElementById("gold-text");
+var radio_count = 0;
+output.innerHTML = slider.value;
+profit.innerHTML = Math.round(slider.value * tsla);
 
+
+slider.oninput = function invest() {
+    output.innerHTML = slider.value;
+    radio_count = check_invest();
+    profit.innerHTML = Math.round(slider.value * radio_count);
+}
+
+var invst_btn = document.getElementsByName('invest-radio');
+
+$(function(){
+  $(invst_btn).click(function() {
+        output.innerHTML = slider.value;
+        radio_count = check_invest();
+        profit.innerHTML = Math.round(slider.value * radio_count);
+  });
+});
+
+function check_invest()
+{
+    for (var j = 0; j < invst_btn.length; j++) {
+        if (invst_btn[j].type == "radio" && invst_btn[j].checked) {
+          if (j == 0){
+            tsla_text.style.display = "block";
+            aapl_text.style.display = "none";
+            gold_text.style.display = "none";
+            return (tsla);
+          }
+          else if (j == 1){
+            tsla_text.style.display = "none";
+            aapl_text.style.display = "block";
+            gold_text.style.display = "none";
+            return (aapl);
+          }
+          else {
+            tsla_text.style.display = "none";
+            aapl_text.style.display = "none";
+            gold_text.style.display = "block";
+            return(gold);
+          }
+        }
+    }
+}
+
+check_invest();
